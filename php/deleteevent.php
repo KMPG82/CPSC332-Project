42413ae2cd56f7086452e932e2d3496adc4f4927
Kevin <kevinponting82@gmail.com>
2024-05-06 23:47:26 -0700

    minor change to delete event sql statement

--- php/deleteevent.php
+++ php/deleteevent.php
@@ -6,10 +6,10 @@ $eventId = $_GET['Event_id'];
 if ($eventId) {
     $sql = "delete event, enrolled_by, presenters, sponsors, keynote_speakers
     from event
-    left join enrolled_by on event.Event_id = enrolled_by.Event_id
-    left join presenters on event.Event_id = presenters.Event_id
-    left join sponsors on event.Event_id = sponsors.Event_id
-    left join keynote_speakers on event.Event_id = keynote_speakers.Event_id
+    join enrolled_by on event.Event_id = enrolled_by.Event_id
+    join presenters on event.Event_id = presenters.Event_id
+    join sponsors on event.Event_id = sponsors.Event_id
+    join keynote_speakers on event.Event_id = keynote_speakers.Event_id
     where event.Event_id = $eventId;";
 
     try {
